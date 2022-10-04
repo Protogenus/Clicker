429124d1db3ec575c1fe2047bb5088c5313eab28
Protogenus <vaporbanks@gmail.com>
2022-10-04 02:20:16 -0500

    Created the worker system. Adjusted some styling.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,23 @@
 // DOM ---
 const woodHeading = document.querySelector("#wood-heading");
 const woodbtn = document.querySelector("#wood-btn");
+const autowood = document.querySelector("#auto-wood");
+const woodWorkerHeading = document.querySelector("#wood-worker-heading");
+
 const copperHeading = document.querySelector("#copper-heading");
 const copperbtn = document.querySelector("#copper-btn");
+const autocopper = document.querySelector("#auto-copper");
+const copperWorkerHeading = document.querySelector("#copper-worker-heading");
+
 const fishHeading = document.querySelector("#fish-heading");
 const fishbtn = document.querySelector("#fish-btn");
+const autofish = document.querySelector("#auto-fish");
+const fishWorkerHeading = document.querySelector("#fish-worker-heading");
 
 // Game variables ---
-let workers = 0;
+let lumberjacks = 0;
+let miners = 0;
+let fisherman = 0;
 
 let wood = 0;
 let woodPerSecond = 0;
@@ -21,40 +31,78 @@ let fishPerSecond = 0;
 // Event listeners ----
 
 // Gathering buttons
-woodbtn.addEventListener("click", function () {
+woodbtn.addEventListener("click", function() {
     wood += 1;
     woodHeading.innerHTML = `Wood: ${wood}`; 
-})
+});
 
-copperbtn.addEventListener("click", function () {
+copperbtn.addEventListener("click", function() {
     copper += 1;
     copperHeading.innerHTML = `Copper: ${copper}`; 
-})
+});
 
-fishbtn.addEventListener("click", function () {
+fishbtn.addEventListener("click", function() {
     fish += 1;
     fishHeading.innerHTML = `Fish: ${fish}`; 
-})
+});
+
+
+// Worker buttons
+autowood.addEventListener("click", function() {
+    woodPerSecond += 1;
+    lumberjacks += 1;
+});
+
+autocopper.addEventListener("click", function() {
+    copperPerSecond += 1;
+    miners += 1;
+});
+
+autofish.addEventListener("click", function() {
+    fishPerSecond += 1;
+    fisherman += 1;
+});
+
+// Workers
+autowood.addEventListener("click", () => {
+    woodPerSecond += 1;
+    }
+);
+
+autocopper.addEventListener("click", () => {
+    copperPerSecond += 1;
+    }
+);
+
+autofish.addEventListener("click", () => {
+    fishPerSecond += 1;
+    }
+);
+
+// Function ---
+function addWoodWorker() {
+    wood += woodPerSecond;
+}
+
+function addCopperWorker() {
+    copper += copperPerSecond;
+}
+
+function addFishWorker() {
+    fish += fishPerSecond;
+}
+
 
 // Game loop ---
 window.setInterval(function () {
-    //autoClicker() Will be used for workers
+    addWoodWorker()
+    addCopperWorker()
+    addFishWorker()
     woodHeading.innerHTML = `Wood: ${wood}`;
     copperHeading.innerHTML = `Copper: ${copper}`;
     fishHeading.innerHTML = `Fish: ${fish}`;
+
+    woodWorkerHeading.innerHTML = `Lumberjacks: ${lumberjacks}`;
+    copperWorkerHeading.innerHTML = `Miners: ${miners}`;
+    fishWorkerHeading.innerHTML = `Fisherman: ${fisherman}`;
 }, 500);
-
-
-/* This will be for adding workers, essentially autoclickers
-autobtn.addEventListener("click", () => {  You can use => instead of writing out 'function()'
-    if(clicks >= 5) {
-        clicks -= 5;
-        clicksPerSecond += 5;
-        autoclickers += 1;
-    }
-});
-
-function autoClicker() {
-    clicks += clicksPerSecond;
-}
-*/

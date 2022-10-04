008e4f990a06473297162551037aeb70118af8a4
Protogenus <vaporbanks@gmail.com>
2022-10-04 14:01:15 -0500

    Implemented store menu, money, ability to sell resources, updated Stockpile profile section, added Google font Mulish, adjusted some styling, and added the Google font Mulish. Tools have not been implemented in this iteration.

--- js/app.js
+++ js/app.js
@@ -1,32 +1,44 @@
+
 // DOM ---
+const moneyHeading = document.querySelector("#money-heading");
+
 const woodHeading = document.querySelector("#wood-heading");
 const woodbtn = document.querySelector("#wood-btn");
 const autowood = document.querySelector("#auto-wood");
 const woodWorkerHeading = document.querySelector("#wood-worker-heading");
 
-const copperHeading = document.querySelector("#copper-heading");
-const copperbtn = document.querySelector("#copper-btn");
-const autocopper = document.querySelector("#auto-copper");
-const copperWorkerHeading = document.querySelector("#copper-worker-heading");
+const oreHeading = document.querySelector("#ore-heading");
+const orebtn = document.querySelector("#ore-btn");
+const autoore = document.querySelector("#auto-ore");
+const oreWorkerHeading = document.querySelector("#ore-worker-heading");
 
 const fishHeading = document.querySelector("#fish-heading");
 const fishbtn = document.querySelector("#fish-btn");
 const autofish = document.querySelector("#auto-fish");
 const fishWorkerHeading = document.querySelector("#fish-worker-heading");
 
+const sellWood = document.querySelector("#sell-wood");
+const sellOre = document.querySelector("#sell-ore");
+const sellFish = document.querySelector("#sell-fish");
+
 // Game variables ---
+let money = 0;
+
 let lumberjacks = 0;
 let miners = 0;
 let fisherman = 0;
 
 let wood = 0;
 let woodPerSecond = 0;
+let woodCost = 20;
 
-let copper = 0;
-let copperPerSecond = 0;
+let ore = 0;
+let orePerSecond = 0;
+let oreCost = 50;
 
 let fish = 0;
 let fishPerSecond = 0;
+let fishCost = 15;
 
 // Event listeners ----
 
@@ -36,15 +48,31 @@ woodbtn.addEventListener("click", function() {
     woodHeading.innerHTML = `Wood: ${wood}`; 
 });
 
-copperbtn.addEventListener("click", function() {
-    copper += 1;
-    copperHeading.innerHTML = `Copper: ${copper}`; 
+orebtn.addEventListener("click", function() {
+    ore += 1;
+    oreHeading.innerHTML = `Ore: ${ore}`; 
 });
 
 fishbtn.addEventListener("click", function() {
     fish += 1;
     fishHeading.innerHTML = `Fish: ${fish}`; 
 });
+
+// Selling buttons
+sellWood.addEventListener("click", function() {
+    money += woodCost * wood;
+    wood = 0;
+})
+
+sellOre.addEventListener("click", function() {
+    money += oreCost * ore;
+    ore = 0;
+})
+
+sellFish.addEventListener("click", function() {
+    money += fishCost * fish;
+    fish = 0;
+})
 
 
 // Worker buttons
@@ -53,8 +81,8 @@ autowood.addEventListener("click", function() {
     lumberjacks += 1;
 });
 
-autocopper.addEventListener("click", function() {
-    copperPerSecond += 1;
+autoore.addEventListener("click", function() {
+    orePerSecond += 1;
     miners += 1;
 });
 
@@ -69,8 +97,8 @@ autowood.addEventListener("click", () => {
     }
 );
 
-autocopper.addEventListener("click", () => {
-    copperPerSecond += 1;
+autoore.addEventListener("click", () => {
+    orePerSecond += 1;
     }
 );
 
@@ -79,13 +107,13 @@ autofish.addEventListener("click", () => {
     }
 );
 
-// Function ---
+// Functions ---
 function addWoodWorker() {
     wood += woodPerSecond;
 }
 
-function addCopperWorker() {
-    copper += copperPerSecond;
+function addoreWorker() {
+    ore += orePerSecond;
 }
 
 function addFishWorker() {
@@ -96,13 +124,14 @@ function addFishWorker() {
 // Game loop ---
 window.setInterval(function () {
     addWoodWorker()
-    addCopperWorker()
+    addoreWorker()
     addFishWorker()
     woodHeading.innerHTML = `Wood: ${wood}`;
-    copperHeading.innerHTML = `Copper: ${copper}`;
+    oreHeading.innerHTML = `Ore: ${ore}`;
     fishHeading.innerHTML = `Fish: ${fish}`;
 
     woodWorkerHeading.innerHTML = `Lumberjacks: ${lumberjacks}`;
-    copperWorkerHeading.innerHTML = `Miners: ${miners}`;
+    oreWorkerHeading.innerHTML = `Miners: ${miners}`;
     fishWorkerHeading.innerHTML = `Fisherman: ${fisherman}`;
+    moneyHeading.innerHTML = `$${money}`
 }, 500);
